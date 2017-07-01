9d443c33489eb1d713ba93ea5fccd9bc0e7b416f
Adam Armour <adam.armour@teamaol.com>
2017-06-30 21:59:35 -0600

    feat(components): project bottom toolbar content

--- src/app/components/app-layout/app-layout.component.ts
+++ src/app/components/app-layout/app-layout.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  selector: 'mad-app-layout-bottom-toolbar',
+  template: '<ng-content></ng-content>'
+})
+export class AppLayoutBottomToolbarComponent {}
+
+@Component({
   selector: 'mad-app-layout',
   templateUrl: './app-layout.component.html',
   styleUrls: ['./app-layout.component.scss']
