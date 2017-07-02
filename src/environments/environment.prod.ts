b2e7cda3c69dd0275fa150ab756c1791312a20c3
Adam Armour <adam.armour@teamaol.com>
2017-07-01 20:04:53 -0600

    chore: add firebase libs and config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyBD1ZoYdTZzv2nBNzwylgIQMA3Djq94SQU',
+    authDomain: 'mapquest-after-dark.firebaseapp.com',
+    databaseURL: 'https://mapquest-after-dark.firebaseio.com',
+    projectId: 'mapquest-after-dark',
+    storageBucket: '',
+    messagingSenderId: '327572573145'
+  },
   mapbox: {
     apiKey: 'pk.eyJ1IjoiYWFybW91ciIsImEiOiJjajRsOXZoanAwcjl0MzNuM21sNWVrN2VoIn0.rcCq4gIcmvnT5lhXxI8WPw'
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,14 @@
 
 export const environment = {
   production: false,
+  firebase: {
+    apiKey: 'AIzaSyBD1ZoYdTZzv2nBNzwylgIQMA3Djq94SQU',
+    authDomain: 'mapquest-after-dark.firebaseapp.com',
+    databaseURL: 'https://mapquest-after-dark.firebaseio.com',
+    projectId: 'mapquest-after-dark',
+    storageBucket: '',
+    messagingSenderId: '327572573145'
+  },
   mapbox: {
     apiKey: 'pk.eyJ1IjoiYWFybW91ciIsImEiOiJjajRsOXZoanAwcjl0MzNuM21sNWVrN2VoIn0.rcCq4gIcmvnT5lhXxI8WPw'
   }
