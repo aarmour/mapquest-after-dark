414a1871faa75d4b28a560ceda3d44aea7440c1f
Adam Armour <adam.armour@teamaol.com>
2017-07-07 22:33:36 -0600

    feat(explore): update url when map extent changes

--- src/app/+explore/explore-container/explore-container.component.ts
+++ src/app/+explore/explore-container/explore-container.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MdDialog } from '@angular/material';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { Store } from '@ngrx/store';
+import { go } from '@ngrx/router-store';
 
 import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 
 import * as selectors from '../../state/explore/selectors';
 import { SetMapExtentAction } from '../../state/explore/actions';
@@ -16,22 +19,35 @@ import { LayerDialogComponent } from '../layer-dialog/layer-dialog.component';
   templateUrl: './explore-container.component.html',
   styleUrls: ['./explore-container.component.scss']
 })
-export class ExploreContainerComponent implements OnInit {
+export class ExploreContainerComponent implements OnDestroy, OnInit {
 
   mapCenter: Observable<mapboxgl.LngLat>;
   mapZoom: Observable<number>;
+  routeParamsSubscription: Subscription;
 
-  constructor(private dialog: MdDialog, private store: Store<State>) { }
+  constructor(private route: ActivatedRoute, private dialog: MdDialog, private store: Store<State>) { }
 
   ngOnInit() {
     this.mapCenter = this.store.select(selectors.mapCenter);
     this.mapZoom = this.store.select(selectors.mapZoom);
+    this.routeParamsSubscription = this.route.paramMap
+      .filter((params: ParamMap) => params.get('x') !== null && params.get('y') !== null && params.get('z') !== null)
+      .map((params: ParamMap) =>
+        ({
+          center: { lng: parseFloat(params.get('x')), lat: parseFloat(params.get('y')) } as mapboxgl.LngLat,
+          zoom: parseFloat(params.get('z'))
+        }))
+      .subscribe((extent) => this.store.dispatch(new SetMapExtentAction(extent)));
+  }
+
+  ngOnDestroy() {
+    this.routeParamsSubscription.unsubscribe();
   }
 
   onMapMoveend($event) {
     const center = $event.target.getCenter();
     const zoom = $event.target.getZoom();
-    this.store.dispatch(new SetMapExtentAction({ center, zoom }));
+    this.store.dispatch(go(['/explore', { x: center.lng, y: center.lat, z: zoom }]));
   }
 
   openLayersDialog() {
