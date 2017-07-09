b323406f83fca80b17d85fb19ceba1dd4f3c6203
Adam Armour <adam.armour@teamaol.com>
2017-07-09 16:42:15 -0600

    feat(user): add user profile information

--- src/app/+user/user-profile-container/user-profile-container.component.ts
+++ src/app/+user/user-profile-container/user-profile-container.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+
+import { Observable } from 'rxjs/Observable';
+
+import * as userSelectors from '../../state/user/selectors';
+import { UserInfo } from '../../state/user/actions';
+import { State } from '../../state/state';
+
 @Component({
   selector: 'mad-user-profile-container',
   templateUrl: './user-profile-container.component.html',
@@ -7,9 +15,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileContainerComponent implements OnInit {
 
-  constructor() { }
+  userProfile: Observable<UserInfo>;
+
+  constructor(private store: Store<State>) { }
 
   ngOnInit() {
+    this.userProfile = this.store.select(userSelectors.profile);
   }
 
 }
--- src/app/+user/user.module.ts
+++ src/app/+user/user.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ComponentsModule } from '../components/components.module';
+
 import { UserRoutingModule } from './user-routing.module';
 import { UserProfileContainerComponent } from './user-profile-container/user-profile-container.component';
 import { UserSettingsContainerComponent } from './user-settings-container/user-settings-container.component';
@@ -8,6 +10,7 @@ import { UserSettingsContainerComponent } from './user-settings-container/user-s
 @NgModule({
   imports: [
     CommonModule,
+    ComponentsModule,
     UserRoutingModule
   ],
   declarations: [
