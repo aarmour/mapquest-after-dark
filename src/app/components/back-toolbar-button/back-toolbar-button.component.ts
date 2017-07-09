96c0d2e8960d272e51340f6afb91d332edbb811b
Adam Armour <adam.armour@teamaol.com>
2017-07-09 16:15:45 -0600

    feat(components): add text and link inputs to back toolbar button

--- src/app/components/back-toolbar-button/back-toolbar-button.component.ts
+++ src/app/components/back-toolbar-button/back-toolbar-button.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'mad-back-toolbar-button',
   templateUrl: './back-toolbar-button.component.html',
   styleUrls: ['./back-toolbar-button.component.scss']
 })
-export class BackToolbarButtonComponent implements OnInit {
+export class BackToolbarButtonComponent {
+
+  @Input() link = '/';
+  @Input() text = 'Back';
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
