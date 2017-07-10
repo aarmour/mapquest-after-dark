a2eb3d3fc1750e3461159aebd8418c8b688cff00
benjamin.click <benjamin.click@mapquest.com>
2017-07-10 13:42:23 -0600

    feat(friends): add basic friend layout

--- src/app/+friends/friends-container/friends-container.component.ts
+++ src/app/+friends/friends-container/friends-container.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FriendsContainerComponent implements OnInit {
 
+  mockFriends: Array<Object>
+
   constructor() { }
 
   ngOnInit() {
+    this.mockFriends = [
+      {
+        avatar: 'http://2.bp.blogspot.com/-PYhX1SBcspc/TWLse_215zI/AAAAAAAB9gE/grEhAa68XiU/s1600/Martin_Van_Buren.jpg',
+        name: 'Martin Van Buren',
+        location: 'Pepsi Center'
+      },
+      {
+        avatar: 'http://cdn.history.com/sites/2/2013/11/James_K_Polk-AB.jpeg',
+        name: 'James K. Polk',
+        loation: 'not shared'
+      },
+      {
+        avatar: 'https://www.biography.com/.image/t_share/MTE5NDg0MDU0OTY0MTc2Mzk5/millard-fillmore-9295008-1-402.jpg',
+        name: 'Millard Fillmore',
+        location: 'Coors Field'
+      }
+    ]
   }
 
 }
